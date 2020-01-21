ae1324d50476e520a4f13c80a1e736409370b5af
小林 <misakura8888@gmail.com>
2020-01-21 16:41:11 +0900

    [課題] Lesson4 ScriptTestプロジェクト
    配列の初期化とfor分を使って出力 + クラスの作成とメンバ変数・メンバ関数の使用

---
+++ Assets/Test.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+	void Start ()
+    {
+        //Lesson4課題
+
+        int[] array = { 2, 5, 3, 6, 4 };
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+
+
+
+        //以下、Lesson4発展課題
+
+        Boss boss = new Boss();
+
+        for (int i = 1; i <= 10; i++)
+        {
+            boss.Magic();
+        }
+
+        boss.Magic();
+    }
+}
+
+public class Boss
+{
+    int mp = 53;
+
+    public void Magic()
+    {
+        this.mp -= 5;
+
+        if (this.mp > 0)
+        {
+            Debug.Log("魔法攻撃をした。残りMPは" + this.mp + "。");
+        } else
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+}
